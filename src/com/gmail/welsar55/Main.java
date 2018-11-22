e09f3f2d1b7b97e54d15ceb796fcf125a87824d2
Welsar55 <welsar55@gmail.com>
2018-11-22 15:11:10 -0600

    Enhanced config

--- src/com/gmail/welsar55/Main.java
+++ src/com/gmail/welsar55/Main.java
@@ -1,7 +1,6 @@
 package com.gmail.welsar55;
 
 import javazoom.jl.decoder.Bitstream;
-import javazoom.jl.decoder.BitstreamException;
 import javazoom.jl.decoder.Header;
 import org.apache.commons.io.FileUtils;
 
@@ -14,20 +13,30 @@ public class Main {
     public String OSUSongsPath = null;
     public String saveFilePath = null;
 
+    public int maxBPM = 360;
+    public int minBPM = 80;
+
+    public int minTimingPoints = 1;
+    public int maxTimingPoints = 50;
+
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Commands: \nrandomize\nconvertall (Might run out of memory atm.)\nconvert [path to .osu file]");
-        Main main = new Main();
-        main.loadConfig();
-        String command = scanner.nextLine();
-        if(command.trim().equalsIgnoreCase("randomize")){
-            main.randomizeMusic();
-        }else if(command.trim().equalsIgnoreCase("convertall")){
-            main.convertEntireOSULibrary();
-        }else if(command.startsWith("convert ")){
-            String filePath = command.substring("convert ".length());
-            File osu = new File(filePath);
-            main.convertOSU(osu, main.getMapSongTitle(osu));
+        while(true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Commands: \nrandomize\nconvertall (Might run out of memory atm.)\nconvert [path to .osu file]\nexit");
+            Main main = new Main();
+            main.loadConfig();
+            String command = scanner.nextLine();
+            if (command.trim().equalsIgnoreCase("randomize")) {
+                main.randomizeMusic();
+            } else if (command.trim().equalsIgnoreCase("convertall")) {
+                main.convertEntireOSULibrary();
+            } else if (command.startsWith("convert ")) {
+                String filePath = command.substring("convert ".length());
+                File osu = new File(filePath);
+                main.convertOSU(osu, main.getMapSongTitle(osu));
+            }else if(command.startsWith("exit")){
+                System.exit(1);
+            }
         }
     }
 
@@ -41,7 +50,7 @@ public class Main {
                         dotOSU = randomDotOSU();
                     }
                     convertOSU(dotOSU, getMapSongTitle(dotOSU));
-                    setCustomSongAs(i, getMapSongTitle(dotOSU) + ".mp3");
+                    setCustomSongAs(i,getMapSongTitle(dotOSU) + ".mp3");
                     exception = false;
                 }
                 catch (Exception e){
@@ -56,11 +65,19 @@ public class Main {
         String line = null;
         while ((line = configReader.readLine()) != null) {
             if (line.startsWith("OSUPath:")){
-                OSUSongsPath = line.substring("osupath:".length());
+                OSUSongsPath = line.substring("OSUPath:".length());
             }else if(line.startsWith("MusicPath:")){
-                customMusicPath = line.substring("musicpath:".length());
+                customMusicPath = line.substring("MusicPath:".length());
             }else if(line.startsWith("SavePath:")){
-                saveFilePath = line.substring("savepath:".length());
+                saveFilePath = line.substring("SavePath:".length());
+            }else if(line.startsWith("MaximumAmountOfTimingPoints:")){
+                maxTimingPoints = Integer.parseInt(line.substring("MaximumAmountOfTimingPoints:".length()));
+            }else if(line.startsWith("MinimumAmountOfTimingPoints:")){
+                minTimingPoints = Integer.parseInt(line.substring("MinimumAmountOfTimingPoints:".length()));
+            }else if(line.startsWith("MaximumBPM:")) {
+                maxBPM = Integer.parseInt(line.substring("MaximumBPM:".length()));
+            }else if(line.startsWith("MinimumBPM:")){
+                minBPM = Integer.parseInt(line.substring("MinimumBPM:".length()));
             }
         }
         if(OSUSongsPath == null || customMusicPath == null || saveFilePath == null){
@@ -94,18 +111,25 @@ public class Main {
         XMLWriter.close();
     }
 
-    public File randomDotOSU(){
-        File songsDirectory = new File(OSUSongsPath);
-        ArrayList<File> potentialFolders = new ArrayList<>();
-        for(File songFolders : songsDirectory.listFiles()){
-            if (songFolders.isDirectory()){
-                potentialFolders.add(songFolders);
+    public File randomDotOSU() throws IOException {
+        for (int i=0;i<50;i++) {
+            File songsDirectory = new File(OSUSongsPath);
+            ArrayList<File> potentialFolders = new ArrayList<>();
+            for (File songFolders : songsDirectory.listFiles()) {
+                if (songFolders.isDirectory()) {
+                    potentialFolders.add(songFolders);
+                }
             }
-        }
-        File songFolder = potentialFolders.get(new Random().nextInt(potentialFolders.size()));
-        for(File songFiles : songFolder.listFiles()){
-            if(songFiles.getName().endsWith(".osu")){
-                return songFiles;
+            File songFolder = potentialFolders.get(new Random().nextInt(potentialFolders.size()));
+            for (File songFiles : songFolder.listFiles()) {
+                if (songFiles.getName().endsWith(".osu")) {
+                    double highestBPM = getMaxBPM(songFiles);
+                    Map<Double, Double> timingPoints = getTimingPoints(songFiles);
+                    if(highestBPM >= minBPM && highestBPM <= maxBPM && timingPoints.size() >= minTimingPoints && timingPoints.size() <= maxTimingPoints) {
+                        return songFiles;
+                    }
+                    break;
+                }
             }
         }
         return null;
@@ -119,9 +143,11 @@ public class Main {
                 for(File songFiles : songFolders.listFiles()){
                     if(songFiles.getName().endsWith(".osu")){
                         try {
-                            convertOSU(songFiles, getMapSongTitle(songFiles));
+                            String mapTitle = getMapSongTitle(songFiles);
+                            convertOSU(songFiles, mapTitle);
                         }catch (Exception e){
-                            System.out.println("Failure to convert a song: " + getMapSongTitle(songFiles) + " Reason: " + e.getMessage());
+                            e.printStackTrace();
+                            //System.out.println("Failure to convert a song: " + getMapSongTitle(songFiles) + " Reason: " + e.getMessage());
                         }
                         break;
                     }
@@ -147,8 +173,10 @@ public class Main {
             }
             iterationCount++;
         }
-        FileUtils.copyFile(getMapMP3(dotOSU), new File(customMusicPath + outputName + ".mp3"));
-        writeOneBeatPerLineInSeconds(customMusicPath + outputName + ".mp3.txt", beats);
+        String fileName = sanitizeFileName(outputName);
+        FileUtils.copyFile(getMapMP3(dotOSU), new File(customMusicPath + fileName + ".mp3"));
+        writeOneBeatPerLineInSeconds(customMusicPath + fileName + ".mp3.txt", beats);
+        System.out.println("Converted: " + outputName);
     }
 
     public ArrayList<Double> getBeatsBetween(double start, double end, double MBB) {
@@ -199,7 +227,7 @@ public class Main {
             } else if (atTiming) {
                 //Offset, Milliseconds per Beat, Meter, Sample Set, Sample Index, Volume, Inherited, Kiai Mode
                 String timingData[] = line.split(",");
-                if (timingData[6].equals("1")) {
+                if (timingData.length > 6 && timingData[6].equals("1")) {
                     timingPoints.put(Double.parseDouble(timingData[0]), Double.parseDouble(timingData[1]));
                 }
             }
@@ -228,9 +256,22 @@ public class Main {
             long tn = 0;
             tn = file.getChannel().size();
             return h.total_ms((int) tn);
-        }catch (BitstreamException e){
-            e.printStackTrace();
+        }catch (Exception e){
+            return 3600000;
         }
-        return 3600000;
+    }
+
+    public String sanitizeFileName(String name){
+        return name.replaceAll("[^\\w| ]", "");
+    }
+
+    public double getMaxBPM(File dotOSU) throws IOException {
+        double maxBPM = 0;
+        for(double timingPoints : getTimingPoints(dotOSU).values()){
+            if(timingPoints > maxBPM){
+                maxBPM = timingPoints;
+            }
+        }
+        return 1/maxBPM*60000;
     }
 }
